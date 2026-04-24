9157ee22fee09858ad70b4f943cd7b1facd92574
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2026-04-24 17:32:56 +0200

    implement custom Button component that can use value

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from "./components/Button/Button.vue";
+</script>
 
 <template>
-  <h1>Web Calculator</h1>
+  <div class="flex flex-col items-center gap-4">
+    <h1>Web Calculator</h1>
+    <Button label="0" :value="'0'" :onClick="(value) => console.log(value)" />
+  </div>
 </template>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+type Props = {
+  label: string;
+  value: string;
+  onClick: (value: string) => void;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex w-fit h-fit">
+    <button
+      @click="props.onClick(props.value)"
+      class="w-12 h-12 sm:w-18 sm:h-18 p-1 my-auto rounded-sm text-md sm:text-xl bg-slate-100 text-slate-900 hover:cursor-pointer hover:bg-slate-300 font-semibold"
+    >
+      {{ props.label }}
+    </button>
+  </div>
+</template>
